683f5cf759da4d03603a3a4ea11f00027deb45d1
Henok Teklu <henokzion@gmail.com>
2019-03-14 20:15:18 +0300

    header created

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import Header from "./Layout/Header";
+
 export default() => {
     return(
         <div>
+            <Header></Header>
             is this working?
         </div>
     )
