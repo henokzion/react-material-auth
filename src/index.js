b4a5b11acb86a0e449f5a33d0dc184a28235cb65
Henok Teklu <henokzion@gmail.com>
2019-03-19 18:43:31 +0300

    form reducer added

--- src/index.js
+++ src/index.js
@@ -2,20 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter, Route } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
 import App from "./components/App";
 import Home from "./components/Home";
 import Signin from "./components/Signin";
 import Signup from "./components/Signup";
+import reducers from "./reducers"
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App>
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/login" component={Signin}/>
-            <Route exact path="/signup" component={Signup}/>
-        </App>
-    </BrowserRouter>
+    <Provider store={createStore(reducers, {})}>
+        <BrowserRouter>
+            <App>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/login" component={Signin} />
+                <Route exact path="/signup" component={Signup} />
+            </App>
+        </BrowserRouter>
+    </Provider>
     , document.querySelector("#root"));
 
 
