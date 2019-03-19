a7392a939d77705d57f8ecd33dae8d7028fdc218
Henok Teklu <henokzion@gmail.com>
2019-03-19 18:30:26 +0300

    redux form added

--- src/components/Signin/index.js
+++ src/components/Signin/index.js
@@ -12,6 +12,7 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import withStyles from '@material-ui/core/styles/withStyles';
+import { reduxForm, Field } from "redux-form";
 
 const styles = theme => ({
   main: {
@@ -61,7 +62,7 @@ function SignIn(props) {
         <form className={classes.form}>
           <FormControl margin="normal" required fullWidth>
             <InputLabel htmlFor="email">Email Address</InputLabel>
-            <Input id="email" name="email" autoComplete="email" autoFocus />
+            <Field component={Input} id="email" name="email" autoComplete="email" autoFocus />
           </FormControl>
           <FormControl margin="normal" required fullWidth>
             <InputLabel htmlFor="password">Password</InputLabel>
