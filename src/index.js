7fdbdc0253f9fac30dfe0187b1f32ef0743e1423
Henok Teklu <henokzion@gmail.com>
2019-03-19 18:08:37 +0300

    signup and signin pages created

--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 import App from "./components/App";
 import Home from "./components/Home";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
 
 ReactDOM.render(
     <BrowserRouter>
         <App>
+            <Route path="/" component={Home}/>
+            <Route path="/signin" component={Signin}/>
+            <Route path="/signup" component={Signup}/>
             <Route path="/" component={Home}/>
         </App>
     </BrowserRouter>
